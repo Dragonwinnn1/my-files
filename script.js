a84b49371730db38fa2a585bdda3d33381db8f38
Dragonwinnn1 <dreamschase202@gmail.com>
2025-11-10 15:51:44 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // ================= CONFIG (TIDAK BERUBAH) =================
-const API_URL = "https://script.google.com/macros/s/AKfycbxso9KHdgArN9ns4qPkHmQwbjh6oxyNfQ3xzsvzT_bYFnux8Mb2-8oSqg82dJT_UJw/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbxhQtVRPcVhdD7i7QCMIleYwp3wqudygiDmed9Y7LPn-FOKv5SzuzTqXjYS8fFCkogv/exec";
 
 let currentUser = null;
 let currentRole = null;
@@ -873,6 +873,7 @@ function switchPage(btn){
 }
 function showLogin(){document.getElementById("loginPage").classList.remove("hidden");document.getElementById("dashboardPage").classList.add("hidden");stopDashboardPolling();}
 function showDashboard(){document.getElementById("loginPage").classList.add("hidden");document.getElementById("dashboardPage").classList.remove("hidden");if(currentRole==="superadmin"){document.getElementById("btnSetting").classList.remove("hidden");} startDashboardPolling();}
+
 
 
 
